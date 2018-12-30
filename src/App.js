45e86f96da89890359ba593ca1eba19466d988b2
Valko Eugene <valkojohn@yandex.ru>
2018-12-30 19:13:13 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import Auth from './components/Auth.js'
+import Home from './components/Home.js'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Router>
+        <div className="App">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/auth/">auth</Link>
+              </li>
+              <li>
+                <Link to="/home/">home</Link>
+              </li>
+            </ul>
+          </nav>
+        
+
+          <Route path="/" exact component={Home} />
+          <Route path="/home/" component={Home} />
+          <Route path="/auth/" component={Auth} />
+        </div>
+      </Router>
+    )
   }
 }
 
-export default App;
+export default App
