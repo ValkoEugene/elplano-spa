4289ae9ff0bff28421fec4331272bfb41bcd767a
Valko Eugene <valkojohn@yandex.ru>
2019-01-19 18:46:22 +0300

    deploy heroku

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+it('some test', () => {
+  expect(2 + 2).toBe(4)
+})
