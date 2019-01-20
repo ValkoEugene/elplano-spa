243a654315ed3e930e2dad5610b56360355769e5
ValkoEugene <zohn94@gmail.com>
2019-01-20 12:10:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-const PORT = 9000
+const PORT = process.env.PORT || 9000
 
 app.use(express.static(path.join(__dirname, 'build')))
 
