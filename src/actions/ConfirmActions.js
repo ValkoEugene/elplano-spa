0dfb0c777e1384c0213ff6939c5e0b4e27866181
Valko Eugene <valkojohn@yandex.ru>
2019-03-03 23:03:40 +0300

    подтверждение аккаунта

---
+++ src/actions/ConfirmActions.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import { setRefreshToken, setToken } from '../plugins/token'
+
+export const CONFIRM_REQUEST = 'CONFIRM_REQUEST'
+export const CONFIRM_SUCCESS = 'CONFIRM_SUCCESS'
+export const CONFIRM_ERROR = 'CONFIRM_ERROR'
+
+export const confirm = confirmation_token => {
+  const params = { confirmation_token }
+
+  return dispatch => {
+    dispatch({ type: CONFIRM_REQUEST })
+
+    return (
+      // Promise.resolve({})
+      axios({
+        method: 'get',
+        url: 'https://elplano-api.herokuapp.com/api/v1/users/confirmation',
+        params,
+        data: {},
+        headers: {
+          'Content-Type': 'application/vnd.api+json',
+        },
+      })
+        // .get('https://elplano-api.herokuapp.com/api/v1/users/confirmation', {
+        //   params,
+        // })
+        .then(() => dispatch({ type: CONFIRM_SUCCESS }))
+        .catch(error => dispatch({ type: CONFIRM_ERROR, payload: error }))
+    )
+  }
+}
